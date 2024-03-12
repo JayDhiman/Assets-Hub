bd57f21ea0f16741dcb8f6b356426aabe8976fbb
JayDhiman <jaydhiman01@gmail.com>
2024-03-12 18:35:00 +0530

    completed the react-table

--- src/Components/Dashboard/AssetsData/AssetsForm.jsx
+++ src/Components/Dashboard/AssetsData/AssetsForm.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import Input from '../../Input';
 
-const AssetsForm = ({ setEmpForm, onAdd, assetId, onUpdate }) => {
-  const { register, handleSubmit } = useForm();
- 
-      //update the current assets
-     const handleFormSubmit = (data) => {
-      onAdd(data)
-      setEmpForm(false) // Close the form after submission
-    };
+const AssetsForm = ({ intialvalue ,onSubmit,onClose}) => {
+  
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: intialvalue || {}, // Use defaultValues to pre-fill the form fields
+  });
+
+  const handleAssetForm = (data) => {
+    onSubmit(data);
+    reset();
+  };
+
+
   return (
     <div className="fixed inset-0 top-0 backdrop-blur-md bg-opacity-30 flex justify-center items-center">
       <div className=" bg-gray-100 rounded-lg shadow-lg p-6 max-w-lg w-full">
         <button
           className="absolute top-2 right-2 text-red-400 text-xl hover:text-red-800"
-          onClick={()=>setEmpForm(false)}>
+          onClick={onClose}>
           X
         </button>
         <h1 className="text-center font-primary font-semibold text-xl my-2">
-          {assetId ? 'UPDATE ASSETS' : 'ADD ASSETS'}
+          {intialvalue ? 'UPDATE ASSETS' : 'ADD ASSETS'}
         </h1>
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <form onSubmit={handleSubmit(handleAssetForm)}>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <Input
               label="CPU:"
@@ -84,7 +88,7 @@ const AssetsForm = ({ setEmpForm, onAdd, assetId, onUpdate }) => {
           <div className="text-center mt-4">
             <button
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-              {assetId ? 'Update' : 'Submit'}
+              {intialvalue ? 'Update' : 'Submit'}
             </button>
           </div>
         </form>
--- src/Pages/Dashboard/Assets.jsx
+++ src/Pages/Dashboard/Assets.jsx
@@ -1,85 +1,233 @@
-import React, {  useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Layout from '../../Components/Dashboard/Layout';
 import Input from '../../Components/Input';
-
 import { IoAddOutline } from 'react-icons/io5';
 import AssetsForm from '../../Components/Dashboard/AssetsData/AssetsForm';
-import AssetsTable from '../../Components/Dashboard/AssetsData/AssetsTable';
-import { useDispatch, useSelector } from 'react-redux';
-import { addAssets,getAssets, updateAssets } from '../../store/AssetsSlice'; 
-
+import axios from 'axios';
+import { useSortBy, useTable } from 'react-table';
+import { MdOutlineDelete } from 'react-icons/md';
+import { RxUpdate } from 'react-icons/rx';
 
 const Assets = () => {
-  const [empForm, setEmpForm] = useState(false); // State for form popup
-  const dispatch = useDispatch();
+  const [assets, setAssets] = useState([]); // state for managing the data
+  const [assetID, setAssetID] = useState(null); // State for selected asset to be updated
+  const [addForm, setAddForm] = useState(false);
+  const [editForm, setEditForm] = useState(false);
+  const [deleteForm, setDeleteForm] = useState(false);
 
-  const handleToggleEmpForm= () => setEmpForm(true)
+  // function for getting the values from the server
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const assets = useSelector((state) => {
-
-    return state.assets.assetsData;
-  });
-
- //update the current asset
-
-
-  const handleUpdateAssets = (assetData)=>{
-   dispatch(updateAssets(assetData))
-   setEmpForm(false)
-  }
-
- // Add asstes
-  const handleAddAsset = (assetData) => {
-    dispatch(addAssets(assetData));
-    setEmpForm(false); // Close the form after adding the asset
+  const fetchData = async () => {
+    try {
+      const res = await axios.get('http://localhost:3000/Assets');
+      setAssets(res.data);
+    } catch (error) {
+      console.log('Error getting the response from the server', error);
+    }
   };
 
-  useEffect(() => {
-    dispatch(getAssets()); // Fetch assets data when component mounts
-  }, [dispatch]);
+  // function for handling the post and put request
+  const handleForm = async (data) => {
+    try {
+      if (assetID) {
+        await axios.put(`http://localhost:3000/Assets/${assetID.id}`, data);
+        setEditForm(false);
+      } else {
+        await axios.post('http://localhost:3000/Assets', data);
+        setAddForm(false);
+      }
+      fetchData();
+      setAssetID(null);
+    } catch (error) {
+      console.log('Error submitting the form', error);
+    }
+  };
+
+  // function for editing the entity
+  const handleEdit = (asset) => {
+    setAssetID(asset);
+    setEditForm(true);
+  };
+
+  const handleDelete = async () => {
+    try {
+      if (assetID) {
+        await axios.delete(`http://localhost:3000/Assets/${assetID.id}`);
+        fetchData(); // Fetch assets after successful deletion
+        setDeleteForm(false); // Close the delete confirmation popup
+      }
+    } catch (error) {
+      console.error('Error deleting asset:', error);
+    }
+  };
+
+  const handleDeleteConfirmation = (asset) => {
+    setAssetID(asset); // Set the asset to delete
+    setDeleteForm(true); // Show the delete confirmation popup
+  };
+
+  // Table
+  const columns = useMemo(
+    () =>
+      Object.keys(assets[0] || {}).map((key) => ({
+        Header: key.toUpperCase(),
+        accessor: key,
+      })),
+    [assets]
+  );
+
+  const tableHooks = (hooks) => {
+    hooks.visibleColumns.push((columns) => [
+      ...columns,
+      {
+        id: 'action',
+        Header: 'Action',
+        Cell: ({ row }) => (
+          <div className="">
+            <button
+              className="px-1 p-1 text-blue-400"
+              onClick={() => handleEdit(row.original)}
+            >
+              <RxUpdate />
+            </button>
+
+            <button className="text-red-500" onClick={() => handleDeleteConfirmation(row.original)}>
+              <MdOutlineDelete />
+            </button>
+          </div>
+        ),
+      },
+    ]);
+  };
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+    {
+      columns,
+      data: assets,
+    },
+    useSortBy,
+    tableHooks
+  );
+
   return (
     <Layout>
-      <div className='flex overflow-auto'>
-        <div className='w-full overflow-hidden'>
-          <div className='m-4 border-b'>
-            <h1 className='text-2xl font-primary mx-1 font-medium '>Assets</h1>
-            <h2 className='uppercase text-[15px] mx-2 mb-2 '>Dashboard / Assets</h2>
+      <div className="flex overflow-auto">
+        <div className="w-full overflow-hidden">
+          <div className="m-4 border-b">
+            <h1 className="text-2xl font-primary mx-1 font-medium ">Assets</h1>
+            <h2 className="uppercase text-[15px] mx-2 mb-2 ">Dashboard / Assets</h2>
           </div>
         </div>
       </div>
 
-      <div className=''>
-        <div className='container mx-auto w-full p-2 '>
-          <div className='flex items-center flex-wrap gap-4 w-full'>
-            <div className='mx-2 max-md:w-full flex-1'>
+      <div className="">
+        <div className="container mx-auto w-full p-2 ">
+          <div className="flex items-center flex-wrap gap-4 w-full">
+            <div className="mx-2 max-md:w-full flex-1">
               <Input placeholder={'Search here'} />
             </div>
 
-            {/* Other input fields and buttons */}
-
-            <div className=''>
+            <div className="">
               <button
-                className='rounded-xl py-[10px] bg-stone-800 text-white px-12  hover:bg-stone-950 flex'
+                className="rounded-xl py-[10px] bg-stone-800 text-white px-12  hover:bg-stone-950 flex"
                 onClick={() => {
-                  setEmpForm(true)
-                }
-              
-                }>
-                <span className='text-2xl px-1'>
+                  setAddForm(true);
+                }}
+              >
+                <span className="text-2xl px-1">
                   <IoAddOutline />
                 </span>
-                <span className='max-sm:hidden'>ADD</span>
+                <span className="max-sm:hidden">ADD</span>
               </button>
             </div>
           </div>
 
-            {empForm && <AssetsForm setEmpForm={ setEmpForm } empForm={empForm } onAdd={handleAddAsset} onUpdate={handleUpdateAssets} assets={assets} />}
+           {/* Table Component */}
 
-          <div className='w-auto h-auto mx-3'>
-            <AssetsTable assets={assets} toggleEmpForm={handleToggleEmpForm}  />
+          <div className="w-auto h-auto mx-3">
+            <div className="mt-12 overflow-auto">
+              <table
+                {...getTableProps()}
+                className="w-full text-sm text-left rtl:text-right text-gray-500 "
+              >
+                <thead className="text-[14px] text-gray-700 uppercase bg-gray-100 ">
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()} className=" ">
+                      {headerGroup.headers.map((column) => (
+                        <th
+                          {...column.getHeaderProps(column.getSortByToggleProps())}
+                          className="p-2 px-3 m-4 "
+                        >
+                          <div className="flex items-center">
+                            <span>{column.render('Header')}</span>
+                            {column.isSorted && (
+                              <span className="ml-1">
+                                {column.isSortedDesc ? ' 🔽' : ' 🔼'}
+                              </span>
+                            )}
+                          </div>
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody {...getTableBodyProps()} className="">
+                  {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                      <tr
+                        {...row.getRowProps()}
+                        className="bg-gray-200 hover:bg-blue-100"
+                      >
+                        {row.cells.map((cell) => (
+                          <td {...cell.getCellProps()} className="p-2 px-3 m-4 ">
+                            {cell.render('Cell')}
+                          </td>
+                        ))}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
+
+
+
+          {addForm && <AssetsForm onSubmit={handleForm} onClose={() => setAddForm(false)} />}
+          {editForm && (
+            <AssetsForm
+            onSubmit={handleForm}
+            intialvalue={assetID} // Pass the selected asset data to the form
+            onClose={() => setEditForm(false)}
+            />
+          )}
         </div>
       </div>
+      {deleteForm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75">
+          <div className="bg-white p-4 rounded-lg">
+            <p>Are you sure you want to delete this asset?</p>
+            <div className="flex justify-end mt-4">
+              <button
+                onClick={() => setDeleteForm(false)} // Close delete confirmation
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded mr-2"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleDelete} // Call handleDelete directly
+                className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </Layout>
   );
 };
